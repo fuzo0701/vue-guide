301b01c1f4a41670e76516db947a8d0fd1cc6fec
leejinmyung <fuzo0701@naver.com>
2021-03-12 15:36:38 +0900

    Vue instans

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="시작합시다 Vue.js"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,36 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>{{ message }}</h2>
+
+    <span v-bind:title="tit_message"> 내 위에 잠시 마우스를 올리면 동적으로 바인딩 된 title을 볼 수 있습니다.</span>
+    <p v-if="seen">이제 나를 볼수 있어요</p>
+
+    <div id="demo">
+      <button v-on:click="show =! show"> Toggle </button>
+      <transition name="fade">
+        <p v-if="show">hello</p>
+      </transition>
+    </div>
+
+    <div id="todoList">
+      <ol>
+        <li v-for="(todo, i) in todos" :key="i">
+         {{ todo.text }}
+        </li>
+      </ol>
+    </div>
+
+    <div id="reverse">
+      <p>{{reverseMsg}}</p>
+      <button v-on:click="reverseMessage">메시지 뒤집기</button>
+    </div>
+
+    <div id="app-6">
+      <p> {{ appMessage }} </p>
+      <input v-model="appMessage">
+    </div>
+    
   </div>
 </template>
 
@@ -35,6 +39,26 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      message : '안녕하세요 Vue!',
+      tit_message : '이 페이지는 ' + new Date() + ' 에 로드 되었습니다.',
+      seen: false,
+      show: true,
+      reverseMsg : '안녕하세요! Vue.js!',
+      todos : [
+        { id:0 , text : 'JavaScript 배우기'},
+        { id:1 , text : 'Vue 배우기'},
+        { id:2 , text : '무언가 멋진 것을 만들기'}
+      ],
+      appMessage : '안녕하세요!! 6'
+    }
+  },
+  methods : {
+    reverseMessage: function(){
+      this.reverseMsg = this.reverseMsg.split('').reverse().join('')
+    }
   }
 }
 </script>
@@ -54,5 +78,13 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
